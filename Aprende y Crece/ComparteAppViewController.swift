2df370827e2d0a7fb3eff395c7791c0e517f7a93
Ana Laura <rgutierrezgonzalez@hotmail.com>
2016-03-15 13:52:17 -0600

    Se agrego funcionalidad en Compartir App

---
+++ Aprende y Crece/ComparteAppViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ComparteAppViewController.swift
+//  Aprende y Crece
+//
+//  Created by Roberto Gutierrez on 15/03/16.
+//  Copyright © 2016 Roberto Gutierrez. All rights reserved.
+//
+
+import UIKit
+import Social
+import MessageUI
+
+class ComparteAppViewController: UIViewController, MFMailComposeViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // ACCIONES BOTONES COMPARTIR
+    
+    @IBAction func compartirEnWhatsapp(sender: AnyObject) {
+        
+        let urlString = "Hola, les recomiendo la App Aprende y crece www.aprendeycrece.com!"
+        let urlStringEncoded = urlString.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())
+        let url  = NSURL(string: "whatsapp://send?text=\(urlStringEncoded!)")
+        
+    }
+    
+    @IBAction func compartirEnFacebook(sender: AnyObject) {
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            var fbShare:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            let url = NSURL(string: "http://www.aprendeycrece.com")
+            fbShare.addURL(url)
+            
+            self.presentViewController(fbShare, animated: true, completion: nil)
+            
+        } else {
+            var alert = UIAlertController(title: "Aprende y crece", message: "Porfavor haga login en facebook en su celular", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+        
+    }
+    
+    @IBAction func compartirEnTwitter(sender: AnyObject) {
+        
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            
+            var tweetShare:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            let url = NSURL(string: "http://www.aprendeycrece.com")
+            tweetShare.addURL(url)
+            tweetShare.setInitialText("Aprende y Crece App")
+            
+            self.presentViewController(tweetShare, animated: true, completion: nil)
+            
+        } else {
+            
+            var alert = UIAlertController(title: "Aprende y crece", message: "Porfavor haga login en Twitter en su celular", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func compartirEnGoogle(sender: AnyObject) {
+        
+        
+    }
+    
+    @IBAction func compartirEnEmail(sender: AnyObject) {
+        
+        let mailComposeViewController = configuredMailComposeViewController()
+        
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+        
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["rgutierrezgonzalez@hotmail.com"])
+        mailComposerVC.setSubject("App aprende y crece")
+        mailComposerVC.setMessageBody("Enlace app aprende y crece http://www.aprendeycrece.com", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "Error", message: "No se pudo enviar su email, intente nuevamente mas tarde", preferredStyle: UIAlertControllerStyle.Alert)
+        sendMailErrorAlert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: MFMailComposeViewControllerDelegate
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        print("Email enviado correctamente")
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
